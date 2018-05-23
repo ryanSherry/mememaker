d240cce83b2bd9e54dc8c6fc9727fe190c06fc79
Ryan Sherry <rsherrycomputers@gmail.com>
2018-05-23 09:55:16 -0700

    Displaying images in grid

--- app/src/main/java/com/teamtreehouse/mememaker/utils/FileUtilities.java
+++ app/src/main/java/com/teamtreehouse/mememaker/utils/FileUtilities.java
@@ -21,7 +21,43 @@ import java.io.OutputStream;
 public class FileUtilities {
 
     public static void saveAssetImage(Context context, String assetName) {
+        File fileDirectory = context.getFilesDir();
+        File fileToWrite = new File(fileDirectory, assetName);
 
+        AssetManager assetManager = context.getAssets();
+        try {
+            InputStream in = assetManager.open(assetName);
+            FileOutputStream out = new FileOutputStream(fileToWrite);
+            copyFile(in,out);
+            in.close();
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void copyFile(InputStream in, OutputStream out) throws IOException {
+        byte[] buffer = new byte[1024];
+        int read;
+        while((read = in.read(buffer)) != -1) {
+            out.write(buffer, 0, read);
+        }
+    }
+
+    public static File [] listFiles(Context context) {
+        File fileDirectory = context.getFilesDir();
+        File [] filteredFiles = fileDirectory.listFiles(new FileFilter() {
+            @Override
+            public boolean accept(File file) {
+                if(file.getAbsolutePath().contains(".jpg")) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        });
+        return filteredFiles;
     }
 
     public static Uri saveImageForSharing(Context context, Bitmap bitmap,  String assetName) {
